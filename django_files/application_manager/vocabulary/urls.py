a8330e94fef0f9992633fcc78f3fda26ecaa85b5
svidovich <samuel.vidovich@gmail.com>
2020-11-11 00:05:16 -0500

    Cleaner endpoint for word

--- django_files/application_manager/vocabulary/urls.py
+++ django_files/application_manager/vocabulary/urls.py
@@ -7,7 +7,7 @@ from vocabulary import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vocabulary/', views.WordList.as_view()),
-    path('vocabulary/word/', views.WordByEnglishVersion.as_view())
+    path('vocabulary/word', views.WordByEnglishVersion.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
