66eac9c3253e21590d7c8dede52dd8d98bc5c672
svidovich <samuel.vidovich@gmail.com>
2020-11-14 20:46:49 -0500

    Update category field to categories, which is many-to-many, allowing for multiple categories for a word

--- django_files/application_manager/vocabulary/models/word.py
+++ django_files/application_manager/vocabulary/models/word.py
@@ -1,6 +1,17 @@
 import uuid
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "categories"
+
+    def __str__(self):
+        return self.name
+
 
 class Words(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -8,7 +19,8 @@ class Words(models.Model):
     english_version = models.TextField()
     translated_version = models.TextField()
     definition = models.TextField(blank=True, null=True)
-    category = models.TextField(blank=True, null=True)
+    category = models.ManyToManyField(Category)
+
 
     class Meta:
         db_table = "words"
