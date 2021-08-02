34e4ecd952bc06509ae42190a0090f6769e07a51
svidovich <samuel.vidovich@gmail.com>
2021-08-01 20:17:51 -0400

    Force language names to be unique

--- django_files/application_manager/vocabulary/models/word.py
+++ django_files/application_manager/vocabulary/models/word.py
@@ -4,7 +4,12 @@ from django.db import models
 
 class Languages(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        db_table = "languages"
+        app_label = "vocabulary"
+        verbose_name_plural = "Languages"
 
 
 class Category(models.Model):
